4e96865558f9c9d20c5b8f6160b959b714f399a1
Koutaro Mukai <mukai.k1011k@outlook.jp>
2023-04-13 21:52:03 +0900

    Enable CORS

--- api/v1/articles.ts
+++ api/v1/articles.ts
@@ -98,13 +98,12 @@ export default async function (
     const summaries = await getArticleSummaries(ARTICLES_URL, count);
     const articles = await Promise.all(summaries.map(url => getArticle(url, ruby)))
 
-    response
-        .setHeader('Access-Control-Allow-Credentials', "true")
-        .setHeader('Access-Control-Allow-Origin', '*')
-        .setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        .setHeader(
+    response.setHeader('Access-Control-Allow-Credentials', "true")
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    response.setHeader(
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-        .status(200).json(articles);
+    )
+    response.status(200).json(articles);
 }
